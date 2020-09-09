3ec191d266b2c5d4fc0f803e92c50018cb629261
Rahul Gupta <swastikmedical74@gmail.com>
2020-09-10 05:07:15 +0530

    Project Termination

--- src/Components/Carouseldisplay.js
+++ src/Components/Carouseldisplay.js
@@ -8,22 +8,22 @@ class Carouseldisplay extends Component {
             <div id='home' className="Carouseldisplay">
                 <div className='container-fluid' >  
                     <Carousel indicators={false}>
-                        <Carousel.Item style={{'height':"85vh"}}>
-                            <img style={{'height':"100%", 'filter':"brightness(15%)"}} alt="Carousel Display" className="d-block w-100" src='assets/img/slider_3.jpg' />
+                        <Carousel.Item className="Carouseldisplay-item" style={{'height':"85vh"}}>
+                            <img style={{'height':"100%", 'filter':"brightness(15%)"}} alt="Carousel Display" className="d-block w-100" src='assets/img/1.jpg' />
                             <Carousel.Caption>
                                 <h3 className='carousel-header'>PLACEMENT SERVICES</h3>
                                 <p className='carousel-info'>At <span className="hrparlour">HRPARLOUR</span> we are comitted to helping candidates to achieve their career goals.</p>
                             </Carousel.Caption>
                         </Carousel.Item>
-                        <Carousel.Item style={{'height':"85vh"}}>
-                            <img style={{'height':"100%", 'filter':"brightness(15%)"}} alt="Carousel Display" className="d-block w-100" src={'assets/img/slider_3.jpg'} />
+                        <Carousel.Item className="Carouseldisplay-item" style={{'height':"85vh"}}>
+                            <img style={{'height':"100%", 'filter':"brightness(15%)"}} alt="Carousel Display" className="d-block w-100" src={'assets/img/2.jpg'} />
                             <Carousel.Caption>    
                                 <h3 className='carousel-header'>KEEP IN TOUCH &amp; STAY UPDATED</h3>
                                 <p className='carousel-info'>We know that Recruitment is a costly and time consuming process. So we focus on ensuring our candidates with high quality permenant career objectives quickly and efficiently.</p>
                             </Carousel.Caption>
                         </Carousel.Item>
-                        <Carousel.Item style={{'height':"85vh"}}>
-                            <img style={{'height':"100%", 'filter':"brightness(15%)"}} alt="Carousel Display" className="d-block w-100" src={'assets/img/slider_3.jpg'} />
+                        <Carousel.Item className="Carouseldisplay-item" style={{'height':"85vh"}}>
+                            <img style={{'height':"100%", 'filter':"brightness(15%)"}} alt="Carousel Display" className="d-block w-100" src={'assets/img/3.jpg'} />
                             <Carousel.Caption>    
                                 <h3 className='carousel-header'>OUR APPROACH</h3>
                                 <p className='carousel-info'>Our consultancy in not just limited to hiring staff, it goes much deeper. At <span className="hrparlour">HRPARLOUR</span> we believe, the success of consultancy firm depends on it's core values and excellent competence of it's Human Resources. So, we aim to provide a solution to hire right talent.</p>
--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -3,18 +3,29 @@ import { Link, animateScroll as scroll } from "react-scroll";
 import '../Styles/Navbar.css'
 
 class Navbar extends Component {
+    navbar = () => {
+        var x = document.getElementById('mynav');
+        if (x.className === 'navbar'){
+            x.className += ' responsive';
+        }else{
+            x.className = 'navbar';
+        }
+    }
     render() {
         return (
-            <div className="navbar">    
+            <div className="navbar" id='mynav'>    
                 <div className="navbar-logo">   
                     <a href='/'><img alt="Hr Parlour Logo" src='assets/img/logo.png' /></a>
                 </div>
+                <div className="navbar-toggle">
+                    <a href="javascript:void(0);" className='icon' onClick={this.navbar}><img className='navbar-toggle-svg' src='assets/svg/down-arrow.svg' /></a>
+                </div>
                 <div className="navbar-list">
                     <ul>
-                        <li><Link to='home' spy={true} hashSpy={true} smooth={true} offset={-70} duration={600}>Home</Link></li>
-                        <li><Link to='about' spy={true} hashSpy={true} smooth={true} offset={-70} duration={600}>About</Link></li>
-                        <li><Link to='clients' spy={true} hashSpy={true} smooth={true} offset={-70} duration={600}>Clients</Link></li>
-                        <li><Link to='contact' spy={true} hashSpy={true} smooth={true} offset={-70} duration={600}>Contact</Link></li>
+                        <li><Link to='home' isDynamic={true} spy={true} hashSpy={true} smooth={true} offset={-70} duration={600}>Home</Link></li>
+                        <li><Link to='about' isDynamic={true} spy={true} hashSpy={true} smooth={true} offset={-70} duration={600}>About</Link></li>
+                        <li><Link to='clients' isDynamic={true} spy={true} hashSpy={true} smooth={true} offset={-70} duration={600}>Clients</Link></li>
+                        <li><Link to='contact' isDynamic={true} spy={true} hashSpy={true} smooth={true} offset={-70} duration={600}>Contact</Link></li>
                     </ul>
                 </div>
             </div>
